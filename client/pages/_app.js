f406a15f7e01c1aa1a539cb530514b85fb2021d8
unknown <anandarizky12>
2021-08-03 07:46:51 +0700

    store

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,14 +1,19 @@
 import Layout from "../components/layout/Layout";
 import { ThemeProvider } from "@material-ui/core/styles";
 import theme from "../src/theme";
+import { Provider, useDispatch, useSelector } from "react-redux";
+import store from "../store/store";
 
 function MyApp({ Component, pageProps }) {
+
   return (
-  <ThemeProvider theme={theme}>
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>  
-  </ThemeProvider>
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>  
+    </ThemeProvider>
+  </Provider>
   )}
 
 export default MyApp
