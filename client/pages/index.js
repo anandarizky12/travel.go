5c160b416409184db6d6747ff4912d83014a8fdc
unknown <anandarizky12>
2022-01-04 20:36:45 +0700

    idec

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,14 +2,18 @@ import React from 'react';
 import Jumbotron from '../components/jumbotron/Jumbotron';
 import Branding from '../components/card_branding/Branding';
 import Content from '../components/Content/Content';
-import { useDispatch, useSelector } from 'react-redux';
+import {  useSelector } from 'react-redux';
 import Admin from '../components/admin/Admin';
 
 export default function Home() {
 
   const user = useSelector(state => state.userLogin);
   const { userInfo } = user;
-
+  const [mounted, setMounted] = React.useState(false)
+  
+  React.useEffect(() =>{  
+    setMounted(true)
+  }, [])
 
     React.useEffect(() => {
         if(!userInfo){
@@ -19,24 +23,19 @@ export default function Home() {
     , [userInfo]);
  
   return (        
- 
-    <div style={{ marginTop : '5px' }} >
-     
-     {userInfo ?
-        userInfo.userData.admin == true ?
-          <div >
-            <Admin />
-          </div>
-          :
-          <div>
-              <Jumbotron/>
-              <Branding/>
-              <Content/>
-          </div>
-     : 
-     <p>Loading</p>
-     }
     
+    <div>
+      {mounted && userInfo && userInfo.userData.admin == true ?
+           
+              <Admin />
+           
+            :
+            <>
+                <Jumbotron/>
+                <Branding/>
+                <Content/>
+            </>
+      }
     </div>  
   )
 }
